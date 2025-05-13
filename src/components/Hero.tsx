52fe57a2553c6eb67c10000d5a9843536f9cee83
lfrzeni <83661257+lfrzeni@users.noreply.github.com>
2025-05-13 18:48:43 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -51,7 +51,7 @@ const Hero = () => {
           <div className="hidden md:block">
             <img 
               src="/lovable-uploads/medico_medbill.png?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-              alt="Médico utilizando sistema MedBill"
+              alt="Médico utilizando o MedBill"
               className="rounded-lg shadow-xl w-full h-auto object-cover"
             />
           </div>
