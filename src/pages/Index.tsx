3566b7bae3b756c43b1871cc9d009b36eed9412c
lfrzeni <83661257+lfrzeni@users.noreply.github.com>
2025-05-14 14:14:52 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
-import Services from '../components/Services';
 import Benefits from '../components/Benefits';
+import Services from '../components/Services';
 import Testimonials from '../components/Testimonials';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
@@ -15,8 +15,8 @@ const Index = () => {
       <Navbar />
       <Hero />
       <About />
-      <Services />
       <Benefits />
+      <Services />
       <Testimonials />
       <Contact />
       <Footer />
